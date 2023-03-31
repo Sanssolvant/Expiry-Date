3237d2792c121a7f9698fa5b7cf10526cc3b1269
Zinedine Tomasella <zinedine.tomasella@hispeed.ch>
2023-03-31 11:33:28 +0200

    mysql

--- app.js
+++ app.js
@@ -18,10 +18,11 @@ const sessionName = 'sid';
 
 // Create Database connection
 const db = mysql.createConnection({
-  host: '127.0.0.1',
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.USEDATABASE,
+  host: 'mysql-ldan',
+  port: '3306',
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 db.connect(err => {
